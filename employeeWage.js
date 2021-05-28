b2cb677d1c362aee6ef57b35707c806c96f39909
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-05-29 00:59:20 +0530

    Calculating employee wage.

--- employeeWage.js
+++ employeeWage.js
@@ -1,9 +1,36 @@
 const IS_ABSENT = 0;
-
+/**
+ * checkRandom employee is absent or present 
+ */
 let empCheck = Math.floor(Math.random() * 10) % 2;
 if (empCheck == IS_ABSENT) {
-    console.log("Employee is Absent");
+    console.log("UC1 : Employee is Absent");
     return;
 } else {
-    console.log("Employee is Present");
+    console.log("UC1 : Employee is Present");
 }
+
+/**
+ * calculating employee wage
+ */
+ const IS_PART_TIME = 1;
+ const IS_FULL_TIME = 2;
+ const PART_TIME_HRS = 4;
+ const FULL_TIME_HRS = 8;
+ const WAGE_PER_HR = 20;
+ 
+let empHrs = 0;
+empWageCheck = Math.floor(Math.random() * 10) % 3;
+switch (empWageCheck) {
+    case IS_PART_TIME :
+        empHrs = PART_TIME_HRS ;
+        break;
+    case IS_FULL_TIME :
+        empHrs = IS_FULL_TIME ;
+        break;
+    default :
+        empHrs = 0;
+}
+
+let empWage = empHrs * WAGE_PER_HR;
+console.log("Employee Wage : " + empWage);
